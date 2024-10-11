4aa3a75e7e27753ceb914911ab11c3c9f29353a4
mauricio <castromac6367@gmail.com>
2024-10-10 21:46:59 -0300

    isolando o código com funções

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,4 +1,6 @@
-﻿//mensagem de boas vindas 
+﻿//criar uma string com a mensagem de boas vindas e mostrar no console
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound!";
+Console.WriteLine("****************************");
 Console.WriteLine(mensagemDeBoasVindas);
+Console.WriteLine("****************************");
 Console.ReadKey();
