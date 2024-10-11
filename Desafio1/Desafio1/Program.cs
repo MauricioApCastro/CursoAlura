94e6e09d26525818f3e8d5841fbb3b3742c3603d
mauricio <castromac6367@gmail.com>
2024-10-10 21:36:04 -0300

    desafio1

---
+++ Desafio1/Desafio1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string curso = "Desenvolvimento de Aplicações com .NET";
+string nome = "Mauricio";
+string sobrenome = "Castro";
+Console.WriteLine("Daniel Portugal e Gui lima");
