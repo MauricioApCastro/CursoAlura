71bee4bbbd71c3d0d46e43eec8df040a5aa875ca
mac <castromac6367@gmail.com>
2024-10-14 13:38:47 -0300

    desafio 6

---
+++ Desafio6/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+float a, b;
+float soma, subtracao, multiplicacao, divisao;
+Console.WriteLine("Digite o primeiro número: ");
+a = float.Parse(Console.ReadLine()!);
+Console.WriteLine("Digite o segundo número: ");
+b = float.Parse(Console.ReadLine()!);
+soma = a + b;
+subtracao = a - b;
+multiplicacao = a * b;
+divisao = a / b;
+Console.WriteLine("Soma: " + soma);
+Console.WriteLine("Subtração: " + subtracao);
+Console.WriteLine("Multiplicação: " + multiplicacao);
+Console.WriteLine("Divisão: " + divisao);
+Console.ReadKey();
+
