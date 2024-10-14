628be68579147ca50d4780cbfb95e9f16c6e170e
mac <castromac6367@gmail.com>
2024-10-14 07:54:59 -0300

    desafio3 ok

---
+++ Desafio2/Desafio3/Desafio3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Digi// See https://aka.ms/new-console-template for more informationte a nota média do aluno: ");
+int notaMedia = int.Parse( Console.ReadLine()!);
+if (notaMedia >= 5)
+{
+    Console.WriteLine("Aprovado");
+}
+else if (notaMedia < 5)
+{
+    Console.WriteLine("Reprovado");
+}
+else
+{
+    Console.WriteLine("Nota inválida");
+}
